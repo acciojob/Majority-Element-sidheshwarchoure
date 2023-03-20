a11a16c603dfb632684e8d9bd6c4b09d06ea34f3
sidheshwarchoure <92964594+sidheshwarchoure@users.noreply.github.com>
2023-03-20 11:07:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your code here
+const nums = [2, 1, 2];
+const result = majorityElement(nums);
+console.log(result); // Output: 2
+
+function majorityElement(nums) {
+  let count = 0;
+  let candidate = null;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (count === 0) {
+      candidate = nums[i];
+    }
+
+    count += nums[i] === candidate ? 1 : -1;
+  }
+
+  return candidate;
+}
